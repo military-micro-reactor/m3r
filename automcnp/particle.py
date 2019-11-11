c3a59078970805afb01383241888a28826dca692
AJ Fleming <aj.fleming@mst.edu>
2019-11-11 17:23:58 -0600

    convert to python module

--- automcnp/particle.py
+++ automcnp/particle.py
@@ -2,11 +2,8 @@ from random import random
 from math import pi as _PI
 from math import hypot
 
-# UNITS FOR MCNP
-# CM-G-SHAKE
-_um = 1.0e-4
-_mcnp_density_factor = -1
-_mcnp_wt_fraction_factor = -1
+from .defaults import *
+
 
 # MEAN KERNEL PARAMETERS -- AGR-2 KERNELS
 _u_enrichment = 0.20  # uranium enrichment wt%
@@ -16,33 +13,7 @@ _kernel_fraction_c = 0.0178  # wt%
 _kernel_fraction_238u = 0.7169  # wt%
 _kernel_fraction_235u = 0.1792  # wt%
 # KERNEL PARAMETER FROM AGR5+ SPEC
-_kernel_radius = 425 * _um
-
-# MEAN PARTICLE PARAMETERS ACCORDING TO SPEC
-# PARTCILES USE SPEC PROPERTIES UNLESS OTHERWISE SPECIFIED
-_nominal_packing_frac = 0.4  # amount of space taken up by fuel particles
-_buffer_thickness = 100 * _um
-_buffer_density = 1.05
-_ipyc_thickness = 40 * _um
-_ipyc_density = 1.9
-_sic_thickness = 35 * _um
-_sic_density = 3.19
-_opyc_thickness = 40 * _um
-_opyc_density = 1.9
-
-# MCNP UNIVERSE, MATERIAL, AND CELL SELECTION
-FIRST_PARTICLE_CELL = 10000
-PARTICLE_CELL_INCREMENT = 10
-FIRST_PARTICLE_UNIVERSE = 10000
-PARTICLE_UNIVERSE_INCREMENT = 1
-FIRST_CELL_UNIVERSE = 50000
-CELL_UNIVERSE_INCREMENT = 1
-FIRST_PARTICLE_SURFACE = 10000
-PARTICLES_SURFACE_INCREMENT = 10
-FIRST_MATERIAL_NUMBER = 1000
-MATERIAL_INCREMENT = 1
-FIRST_TRANSFORM_NUMBER = 100
-TRANSFORM_INCREMENT = 1
+_kernel_radius = 425 * MICRON
 
 
 class Particle:
@@ -58,14 +29,14 @@ class Particle:
                                                            _kernel_fraction_c),
                               (92238, _kernel_fraction_238u),
                               (92235, _kernel_fraction_235u)),
-                 buffer_density=_buffer_density,
-                 buffer_thickness=_buffer_thickness,
-                 ipyc_density=_ipyc_density,
-                 ipyc_thickness=_ipyc_thickness,
-                 sic_density=_sic_density,
-                 sic_thickness=_sic_thickness,
-                 opyc_density=_opyc_density,
-                 opyc_thickness=_opyc_thickness,
+                 buffer_density=BUFFER_DENSITY,
+                 buffer_thickness=BUFFER_THICKNESS,
+                 ipyc_density=IPYC_DENSITY,
+                 ipyc_thickness=IPYC_THICKNESS,
+                 sic_density=SIC_DENSITY,
+                 sic_thickness=SIC_THICKNESS,
+                 opyc_density=OPYC_DENSITY,
+                 opyc_thickness=OPYC_THICKNESS,
                  use_weight_frac=True):
         self.kernel_rad = kernel_rad
         self.kernel_spec = kernel_spec
@@ -88,6 +59,11 @@ class Particle:
         self.dz = 0
 
     def set_origin(self, x, y, z):
+        """
+        Sets the origin of the particle to the ordered triple (x, y, z)
+        Paramters:
+        x, y, z
+        """
         self.dx = x
         self.dy = y
         self.dz = z
@@ -96,6 +72,13 @@ class Particle:
         return _PI * (4 / 3) * (self.total_radius)
 
     def intersects(self, other):
+        """
+        Check to see if this particle intersects another particle.
+        Parameters:
+        other: a particle object
+        Returns: 
+        True if the two particles intersect.
+        """
         return (hypot(
             hypot(self.dx - other.dx, self.dy - other.dy), self.dz - other.dz)
                 <= (self.total_radius + other.total_radius))
@@ -121,18 +104,22 @@ class Particle:
         mcard = material_card.format(
             prefix="m{} ".format(self.kernel_material),
             zaid=self.kernel_spec[0][0],
-            density=_mcnp_density_factor * self.kernel_spec[0][1])
+            density=MCNP_DENSITY_FACTOR * self.kernel_spec[0][1])
         for i in range(1, len(self.kernel_spec)):
             mcard = mcard + material_card.format(
                 prefix="\n      ",
                 zaid=self.kernel_spec[i][0],
-                density=_mcnp_density_factor * self.kernel_spec[i][1])
+                density=MCNP_DENSITY_FACTOR * self.kernel_spec[i][1])
         return mcard
 
     def mcnp_surfaces(self, surface_n, transform_n=0):
         """
         Generates a list of MCNP surface cards for this particle.
-        Each particle requires 4 surfaces.
+        Each particle requires 5 surfaces.
+        Paramters:
+        surface_n: the surface number to start numbering the particle surfaces
+        (optional) transform_n: the particle transformation number. 
+        Will use self.transform_n if the mcnp_transform method has been called.
         """
         if transform_n == 0:
             try:
@@ -191,24 +178,20 @@ class Particle:
             cell_card.format(
                 n=self.cells[0],
                 m=self.kernel_material,
-                rho=_mcnp_density_factor * self.kernel_density,
+                rho=MCNP_DENSITY_FACTOR * self.kernel_density,
                 geom=-1 * self.surfaces[0],
                 u=universe_n))
+        materials = [buffer_mat_n, ipyc_mat_n, sic_mat_n,
+                     opyc_mat_n]
+        densities = [self.buffer_density, self.ipyc_density,
+                   self.sic_density, self.opyc_density]
         for i in range(1, 4):
             cells.append(
                 cell_card.format(
                     n=self.cells[i],
-                    m=self.kernel_material,
-                    rho=_mcnp_density_factor * self.kernel_density,
+                    m=materials[i-1],
+                    rho=MCNP_DENSITY_FACTOR * densities[i-1],
                     geom="{} {}".format(-1 * self.surfaces[i],
                                         self.surfaces[i - 1]),
                     u=universe_n))
         return cells
-
-
-p = Particle()
-p.set_origin(10, 10, 10)
-print(p.mcnp_transform(FIRST_TRANSFORM_NUMBER))
-print(p.mcnp_kernel_material(100))
-print(p.mcnp_surfaces(FIRST_PARTICLE_SURFACE))
-print(p.mcnp_cells(FIRST_PARTICLE_CELL, FIRST_PARTICLE_UNIVERSE))
