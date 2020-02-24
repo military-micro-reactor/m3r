e03afef174faf621a75f38101c0a4b373e7667a8
Alexander Fleming <atrain99@mchsi.com>
2020-02-24 14:16:33 -0600

    Split Model

---
+++ k_eff/splitmodel/plot_energy_spectrum.py
@@ -0,0 +1,21 @@
+from mcnptools import Mctal
+tallyfile = Mctal("mctan")
+
+import numpy as np
+import matplotlib.pyplot as plot
+from matplotlib import cm
+from statistics import mean
+
+tally204 = tallyfile.GetTally(204)
+
+ebins = tally204.GetEBins()
+# F D U S M C E T
+vals = [tally204.GetValue(0, 0, 0, 0, 0, 0, i, 0) for i in range(len(ebins))]
+
+fig, ax = plot.subplots()
+ax.plot(ebins, vals)
+ax.set_xlabel("Energy, MeV", fontsize=14)
+ax.set_ylabel("Flux per source particle rate", fontsize=14)
+plot.xscale('log')
+fig.tight_layout()
+plot.show()
